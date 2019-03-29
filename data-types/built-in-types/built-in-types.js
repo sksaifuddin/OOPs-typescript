d28097a44c43629a5f0c0d49d6be66dab885bc02
sksaifuddin <atushaik@gmail.com>
2019-03-30 01:14:16 +0530

    completed array types

--- data-types/built-in-types/built-in-types.js
+++ data-types/built-in-types/built-in-types.js
@@ -1,3 +1,10 @@
-// Any type
-var myVariable = 'Hello World';
-console.log(myVariable);
+// 1)string type
+var myStringVar = 'Hello world';
+// 2) Number Type
+var myNumberVar = 72;
+// 3) Boolean
+var myBooleanVar = true;
+// The null and undefined cannot be used to reference the data type of a variable. They can only be assigned as values to a variable.
+console.log(myStringVar);
+console.log(myNumberVar);
+console.log(myBooleanVar);
---
+++ data-types/user-defined-types/arrays/arrays.ts
@@ -0,0 +1,12 @@
+// declaring arrays 
+
+const myArr1: number[] = [];
+const myArr2: number[] = new Array();
+const myArr3: number[] = Array();
+
+// best way to delcare a array with its type;
+let myArr4: Array<string>
+
+myArr4[0] = "Saif";
+
+console.log(myArr4);
